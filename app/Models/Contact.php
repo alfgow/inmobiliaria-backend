50179cbdc32200627ed2d621df002f3db12f20c8
alfgow <64243346+alfgow@users.noreply.github.com>
2025-09-24 23:19:42 -0600

    Fix contactos route name

---
+++ app/Models/Contact.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+
+    protected $table = 'contactos';
+
+    protected $fillable = [
+        'name',
+        'nombre',
+        'email',
+        'phone',
+        'telefono',
+        'message',
+        'mensaje',
+    ];
+}
